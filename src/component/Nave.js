589cb5c05e1d51e86018b6dc7d75d3c07023c13f
hesham156 <h456ad@gmail.com>
2023-02-04 16:33:37 +0200

    work

--- src/component/Nave.js
+++ src/component/Nave.js
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import  logo from '../asset/img/logo.png'
+import {AiOutlineShoppingCart} from 'react-icons/ai'
+import {FaUserCircle} from 'react-icons/fa'
 const Nave = () => {
   return (
-    <nav className='nave'>Nave</nav>
+    <nav className='nave '>
+      <div className='container center'>
+          <div className='logo w-25'><h1><img src={logo}/></h1></div>
+          <div className='search w-50'>
+               <div className='searchBar'>
+                       <input type="text" placeholder='Search'/>
+               </div>
+          </div>
+          <div className='lnks w-25'>
+          <AiOutlineShoppingCart/>
+          <FaUserCircle/>
+          </div>
+      </div>
+      <div className='container categor'>
+
+      </div>
+    </nav>
   )
 }
 
