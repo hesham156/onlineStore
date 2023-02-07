79c5dbe540864b3c52658a9c5eecb23bdb6b2de0
hesham156 <h456ad@gmail.com>
2023-02-08 01:19:35 +0200

    new

--- src/component/Nave.js
+++ src/component/Nave.js
@@ -5,12 +5,19 @@ const Nave = () => {
     <nav className='nave w-100'>
       <div className='container'>
         <div className='naveTop w-100'>
-          <div className='logo'></div>
-          <div className='search'></div>
-          <div className='user'></div>
+          <div className='logo'><h2>MRROBOT</h2></div>
+          <div className='search'><form><input type="text" placeholder='Search'/><input type="submit" value="search"/></form></div>
+          <div className='user'><ul><li><a href='#'>cart</a></li><li><a href='#'>user</a></li><li><a href='#'>cart</a></li></ul></div>
           </div>
           <div className='naveBottom w-100'>
-            <div className='category'></div>
+            <div className='category'>
+              <ul>
+              <li><a href='#'>sales</a></li>
+              <li><a href='#'>chose</a></li>
+              <li><a href='#'>men</a></li>
+              <li><a href='#'>women</a></li>
+              </ul>
+            </div>
           </div>
       </div>
     </nav>
